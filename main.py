202e30369a0aca7f355a0512762656291e468c1c
jiananli306 <hwei@wpi.edu>
2021-03-23 20:40:24 -0400

    add splash screen

--- main.py
+++ main.py
@@ -3,16 +3,36 @@ import sys
 from PyQt5 import QtWidgets
 
 # Press the green button in the gutter to run the script.
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QLabel
+
 from MainWindow import MainWindow
 from interfaces.InferenceInterface import InferenceInterface
 
+from PyQt5.QtCore import Qt
+
 if __name__ == '__main__':
     # Define the sensor interfaces
-    inference_interface = InferenceInterface()
+
 
     # load the qt application
     app = QtWidgets.QApplication(sys.argv)
+
+    # splash screen
+    splash = QLabel()
+    pixmap = QPixmap('media/logo/John.png')
+    pixmap = pixmap.scaled(640, 640)
+    splash.setPixmap(pixmap)
+    splash.setWindowFlags(Qt.SplashScreen | Qt.FramelessWindowHint)
+    splash.show()
+
+    # main window init
+    inference_interface = InferenceInterface()
     window = MainWindow(inference_interface=inference_interface)
+
+    # splash screen destroy
+    splash.destroy()
+
     window.show()
     app.exec_()
     print('Resuming Console Interaction.')
