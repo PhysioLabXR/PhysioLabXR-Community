5ae87edf2517b978bf82c9633bbd991812980610
Ziheng Li <S-Vector.Lee@hotmail.com>
2021-03-25 21:47:49 -0400

    add create video function; add only_stream parameter in stream_in

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,5 +1,7 @@
 import os
+from pathlib import Path
 
+import cv2
 import numpy as np
 
 
@@ -69,7 +71,7 @@ class RNStream:
         out_file.close()
         return len(magic + stream_label_bytes + dtype_bytes + dim_bytes + shape_bytes + data_bytes + ts_bytes)
 
-    def stream_in(self, ignore_stream=()):
+    def stream_in(self, ignore_stream=None, only_stream=None):
         total_bytes = float(os.path.getsize(self.fn))  # use floats to avoid scalar type overflow
         buffer = {}
         read_bytes_count = 0.
@@ -107,7 +109,9 @@ class RNStream:
                 data_array_num_bytes = np.prod(shape) * np.dtype(stream_dytpe).itemsize
                 timestamp_array_num_bytes = shape[-1] * np.dtype(ts_dtype).itemsize
 
-                if stream_label not in ignore_stream:
+                this_in_only_stream = (stream_label in only_stream) if only_stream else True
+                not_ignore_this_stream = (stream_label not in ignore_stream) if ignore_stream else True
+                if not_ignore_this_stream and this_in_only_stream:
                     # read data array
                     read_bytes = file.read(data_array_num_bytes)
                     read_bytes_count += len(read_bytes)
@@ -127,3 +131,83 @@ class RNStream:
                     read_bytes_count += data_array_num_bytes + timestamp_array_num_bytes
         print("Stream-in completed: {0}".format(self.fn))
         return buffer
+
+    def get_stream_names(self):
+        total_bytes = float(os.path.getsize(self.fn))  # use floats to avoid scalar type overflow
+        stream_names = []
+        read_bytes_count = 0.
+        with open(self.fn, "rb") as file:
+            while True:
+                print('Scanning stream in progress {}%'.format(str(round(100 * read_bytes_count/total_bytes, 2))), sep=' ', end='\r', flush=True)
+                # read magic
+                read_bytes = file.read(len(magic))
+                read_bytes_count += len(read_bytes)
+                if len(read_bytes) == 0:
+                    break
+                try:
+                    assert read_bytes == magic
+                except AssertionError:
+                    raise Exception('Data invalid, magic sequence not found')
+                # read stream_label
+                read_bytes = file.read(max_label_len)
+                read_bytes_count += len(read_bytes)
+                stream_label = str(read_bytes, encoding).strip(' ')
+                # read read_bytes
+                read_bytes = file.read(max_dtype_len)
+                read_bytes_count += len(read_bytes)
+                stream_dytpe = str(read_bytes, encoding).strip(' ')
+                # read number of dimensions
+                read_bytes = file.read(dim_bytes_len)
+                read_bytes_count += len(read_bytes)
+                dims = int.from_bytes(read_bytes, 'little')
+                # read number of np shape
+                shape = []
+                for i in range(dims):
+                    read_bytes = file.read(shape_bytes_len)
+                    read_bytes_count += len(read_bytes)
+                    shape.append(int.from_bytes(read_bytes, 'little'))
+
+                data_array_num_bytes = np.prod(shape) * np.dtype(stream_dytpe).itemsize
+                timestamp_array_num_bytes = shape[-1] * np.dtype(ts_dtype).itemsize
+
+                file.read(data_array_num_bytes + timestamp_array_num_bytes)
+                read_bytes_count += data_array_num_bytes + timestamp_array_num_bytes
+
+                stream_names.append(stream_label)
+        print("Scanning stream completed: {0}".format(self.fn))
+        return stream_names
+
+    def generate_video(self, video_stream_name, output_path=''):
+        """
+        if output path is not specified, the output video will be place in the same directory as the
+        stream .dats file with a tag to its stream name
+        :param stream_name:
+        :param output_path:
+        """
+        data_fn = self.fn.split('/')[-1]
+        data_root = Path(self.fn).parent.absolute()
+        data = self.stream_in(only_stream=(video_stream_name,))
+
+        video_frame_stream = data[video_stream_name][0]
+        frame_count = video_frame_stream.shape[-1]
+
+        timestamp_stream = data[video_stream_name][1]
+        frate = len(timestamp_stream) / (timestamp_stream[-1] - timestamp_stream[0])
+        try:
+            assert len(video_frame_stream.shape) == 4 and video_frame_stream.shape[2] == 3
+        except AssertionError:
+            raise Exception('target stream is not a video stream. It does not have 4 dims (height, width, color, time)'
+                            'and/or the number of its color channel does not equal 3.')
+        frame_size = (data[video_stream_name][0].shape[1], data[video_stream_name][0].shape[0])
+        output_path = os.path.join(data_root, '{0}_{1}.avi'.format(data_fn.split('.')[0], video_stream_name)) if output_path == '' else output_path
+
+        out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'DIVX'),frate, frame_size)
+
+        for i in range(frame_count):
+            print('Creating video progress {}%'.format(str(round(100 * i / frame_count, 2))), sep=' ', end='\r',
+                  flush=True)
+            img = video_frame_stream[:, :, :, i]
+            # img = np.reshape(img, newshape=list(frame_size) + [-1,])
+            out.write(img)
+
+        out.release()
