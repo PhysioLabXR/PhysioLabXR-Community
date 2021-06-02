aa86e46ef2ccb837e48a554d6ccc7ca2023ad0cd
HaowenWeiJohn <hwei@wpi.edu>
2021-06-02 01:34:50 -0400

    test lsl timestamp

--- utils/IndexPen_utils/indexPen_traning_test.py
+++ utils/IndexPen_utils/indexPen_traning_test.py
@@ -27,14 +27,10 @@ import os
 
 
 DataStreamName = 'TImmWave_6843AOP'
-data_dir_path = 'C:\Recordings\John_test_A-E'
+data_dir_path = 'C:\Recordings\John_error_test_A-E'
 exp_info_dict_json_path = 'C:/Users/HaowenWeiJohn/PycharmProjects/RealityNavigation/utils/IndexPen_utils/IndexPenExp.json'
 
-import os
 
-os.environ['NUMBAPRO_NVVM']    = r'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\nvvm\bin\nvvm64_31_0.dll'
-
-os.environ['NUMBAPRO_LIBDEVICE'] = r'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\nvvm\libdevice'
 
 reshape_dict = {
     'TImmWave_6843AOP': [(8, 16, 1), (8, 64, 1)]
@@ -59,53 +55,7 @@ X_mmw_rA_train, X_mmw_rA_test, Y_train, Y_test = train_test_split(X_mmw_rA, Y, t
 
 
 model = make_model_leo(classes=[1, 2, 3, 4, 5], points_per_sample=120)
-# encoder1 = Sequential()
-# encoder1.add(tf.keras.layers.InputLayer(input_shape=(120, 8, 16, 1)))
-# encoder1.add(TimeDistributed(Conv2D(filters=8, kernel_size=(2, 3),
-#                                     data_format='channels_last')))
-# encoder1.add(TimeDistributed(tf.keras.layers.LeakyReLU(alpha=0.1)))
-#
-# encoder1.add(TimeDistributed(MaxPooling2D(pool_size=2)))
-# encoder1.add(TimeDistributed(BatchNormalization()))
-# encoder1.add(TimeDistributed(Flatten()))  # or Flatten()
-# encoder1.add(TimeDistributed(Dense(32, activation='relu')))
-# # encoder1.add(TimeDistributed(Dropout(rate=0.2)))
-#
-# # ENcoder2
-# encoder2 = Sequential()
-# encoder2.add(tf.keras.layers.InputLayer(input_shape=(120, 8, 64, 1)))
-# encoder2.add(TimeDistributed(Conv2D(filters=16, kernel_size=(3, 3),
-#                                     data_format='channels_last')))
-# encoder2.add(TimeDistributed(tf.keras.layers.LeakyReLU(alpha=0.1)))
-# encoder2.add(TimeDistributed(MaxPooling2D(pool_size=2)))
-# encoder2.add(TimeDistributed(BatchNormalization()))
-# encoder2.add(TimeDistributed(Flatten()))  # or Flatten()
-# encoder2.add(TimeDistributed(Dense(64, activation='relu')))
-# # encoder2.add(TimeDistributed(Dropout(rate=0.2)))
-#
-# merged = concatenate([encoder1.output, encoder2.output])
-# # merged_out = LSTM(32, return_sequences=True, kernel_initializer='random_uniform',
-# #                   kernel_regularizer=tf.keras.regularizers.l2(l=1e-4),
-# #                   recurrent_regularizer=tf.keras.regularizers.l2(l=1e-5),
-# #                   activity_regularizer=tf.keras.regularizers.l2(l=1e-5)
-# #                   )(merged)
-# # merged_out = Dropout(rate=0.2)(merged_out)
-# merged_out = LSTM(32,
-#                   kernel_initializer='random_uniform',
-#                   return_sequences=False,
-#                   )(merged)
-# merged_out = Dropout(rate=0.2)(merged_out)
-# merged_out = Dense(256,
-#                    activation='relu'
-#                    )(merged_out)
-# merged_out = Dropout(rate=0.2)(merged_out)
-# merged_out = Dense(5, activation='softmax', kernel_initializer='random_uniform')(merged_out)
-# # encoder1.build((None, 120, 8, 16, 1))
-# # encoder1.summary()
-# model = Model(inputs=[encoder1.input, encoder2.input], outputs=merged_out)
-#
-# adam = tf.keras.optimizers.Adam(lr=1e-3, decay=1e-7)
-# model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+
 
 es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=10)
 mc = ModelCheckpoint(
