db0b29b2d6649803dcbaebec4877d59a8169e049
HaowenWeiJohn <hwei@wpi.edu>
2021-09-16 16:21:44 -0400

    subject 1 test finish

--- utils/IndexPen_utils/user_study2_data_extraction.py
+++ utils/IndexPen_utils/user_study2_data_extraction.py
@@ -6,8 +6,8 @@ import pickle
 user_study2_data_dir = 'C:/Recordings/user_study2_data'
 user_study2_data_save_dir = 'C:/Users/Haowe/PycharmProjects/IndexPen_Training/data/IndexPenData/IndexPenStudyData/UserStudy2Data'
 # user_study2_data_save_dir = 'D:/IndexPen_data_Study1_Study2/user_study2_data/'
-participant_dir = 'participant_2'
-session_dir = 'session_4'
+participant_dir = 'participant_1'
+session_dir = 'session_5'
 
 full_session_dir_path = os.path.join(user_study2_data_dir, participant_dir, session_dir)
 
