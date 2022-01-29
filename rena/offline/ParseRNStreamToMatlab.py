582fe88f9bbacce30fdd30e51a4a2e692eec176d
ApocalyVec <s-vector.lee@hotmail.com>
2022-01-28 22:48:11 -0500

    update rena data format with VarjoEyeTrackingComplete

--- rena/offline/ParseRNStreamToMatlab.py
+++ rena/offline/ParseRNStreamToMatlab.py
@@ -8,7 +8,7 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
-from rena.utils.data_utils import RNStream
+from utils.data_utils import RNStream
 
 
 
