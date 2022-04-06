71995297406acfd956557ac5f8a5bc61a3789095
JP Suh <junepyo.suh@gmail.com>
2022-04-05 22:08:39 -0500

    InferenceTab WIP

--- rena/ui/InferenceTab.py
+++ rena/ui/InferenceTab.py
@@ -6,6 +6,7 @@ from PyQt5 import QtWidgets, uic
 
 import numpy as np
 from datetime import datetime
+from abc import ABC, abstractmethod
 
 from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QFileDialog
@@ -13,6 +14,20 @@ from PyQt5.QtWidgets import QFileDialog
 from rena import config
 from rena.utils.data_utils import RNStream
 from rena.utils.ui_utils import dialog_popup
+
+
+class RealTimeModel(ABC):
+    """
+    An abstract class for implementing inference models.
+    """
+    def preprocess(self, x, **kwargs):
+        pass
+
+    def predict(self, x, **kwargs):
+        pass
+
+    def prepare_model(self, model_path, preprocess_params_path):
+        pass
 
 
 class InferenceTab(QtWidgets.QWidget):
@@ -31,4 +46,27 @@ class InferenceTab(QtWidgets.QWidget):
             self.model_file_path = selected_model_file
 
     def start_inference_btn_pressed(self):
+        pass
+
+    def process_on_tick(self, window_size, time_step):
+        """
+        Receive data from buffer.
+        Slice the buffer depending on the window size and time step.
+        Preprocess the buffer & model.
+        Predict the result.
+        Emit the data to the LSL outlet.
+        """
+        pass
+
+    def update_buffers(self, data_dict: dict):
+        lsl_data_type = data_dict['lsl_data_type']  # get the type of the newly-come data
+        if lsl_data_type not in self.recording_buffer.keys():
+            self.recording_buffer[lsl_data_type] = [np.empty(shape=(data_dict['frames'].shape[0], 0)),
+                                                    np.empty(shape=(0,))]  # data first, timestamps second
+
+        buffered_data = self.recording_buffer[data_dict['lsl_data_type']][0]
+        buffered_timestamps = self.recording_buffer[data_dict['lsl_data_type']][1]
+
+        self.recording_buffer[lsl_data_type][0] = np.concatenate([buffered_data, data_dict['frames']], axis=-1)
+        self.recording_buffer[lsl_data_type][1] = np.concatenate([buffered_timestamps, data_dict['timestamps']])
         pass
