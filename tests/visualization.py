8b68ac2f89d9cda218d0457504a672f9d6d771f9
Ziheng(Leo) Li <s-vector.lee@hotmail.com>
2022-07-08 06:28:31 -0400

    add qt unit tests

---
+++ tests/visualization.py
@@ -0,0 +1,7 @@
+# reference taken from https://github.com/jmcgeheeiv/pyqttestexample/blob/master/src/MargaritaMixerTest.py
+
+import sys
+import unittest
+from PyQt5.QtGui import QApplication
+from PyQt5.QtTest import QTest
+from PyQt5.QtCore import Qt
