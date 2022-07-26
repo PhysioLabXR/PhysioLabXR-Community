cf5a74b28cb9b76f4f3eec5dc799158298dd3940
ApocalyVec <zl2990@columbia.edu>
2022-07-26 19:59:06 -0400

    add pending settings in stream_shared.py

--- stream_shared.py
+++ stream_shared.py
@@ -4,4 +4,4 @@ This file contains the global shared variables relating to the streams
 """
 from pylsl import ContinuousResolver
 
-lsl_continuous_resolver = ContinuousResolver(forget_after=2)
+lsl_continuous_resolver = ContinuousResolver(forget_after=2)  # TODO add forget_after to settings, a stream will be marked unavailable after 2 seconds
