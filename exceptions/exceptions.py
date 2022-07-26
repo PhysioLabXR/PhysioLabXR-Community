633f59fc5cef6496317502756ce69731ededfe9d
ApocalyVec <zl2990@columbia.edu>
2022-07-26 16:27:09 -0400

    update rena exceptions

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -1,9 +1,14 @@
 class RenaError(Exception):
+    def __init__(self, message=""):
+        self.message = message
+        super().__init__(self.message)
     """Base class for other exceptions"""
     pass
 
 
 class DataPortNotOpenError(RenaError):
+    def __init__(self):
+        super().__init__()
     def __str__(self):
         return 'attempting to read from unopened data port'
 
@@ -12,6 +17,9 @@ class DataPortNotOpenError(RenaError):
 
 
 class PortsNotSetUpError(RenaError):
+    def __init__(self):
+        super().__init__()
+
     def __str__(self):
         return 'CLI Ports are not set up'
 
@@ -20,6 +28,8 @@ class PortsNotSetUpError(RenaError):
 
 
 class GeneralMmWError(RenaError):
+    def __init__(self):
+        super().__init__()
     def __str__(self):
         return 'general mmWave error occurred, please debug the tlv buffer and decoder'
 
@@ -28,6 +38,8 @@ class GeneralMmWError(RenaError):
 
 class BufferOverFlowError(RenaError):
     """Raised when data buffer overflows """
+    def __init__(self):
+        super().__init__()
 
     def __str__(self):
         return 'TLV buffer overflowed'
@@ -37,7 +49,8 @@ class BufferOverFlowError(RenaError):
 
 class InterfaceNotExistError(RenaError):
     """Rasied when an interface doesn't exist when in use"""
-
+    def __init__(self):
+        super().__init__()
     def __str__(self):
         return 'Interface missing...'
 
@@ -46,7 +59,8 @@ class InterfaceNotExistError(RenaError):
 
 class StoragePathInvalidError(RenaError):
     """Raised when the path given is invalid"""
-
+    def __init__(self):
+        super().__init__()
     def __str__(self):
         return 'Invalid path...'
 
@@ -55,20 +69,36 @@ class StoragePathInvalidError(RenaError):
 
 class LeapPortTimeoutError(RenaError):
     """Raised when LeapInterface is running without LeapMouse"""
-
+    def __init__(self):
+        super().__init__()
     def __str__(self):
         return 'LeapMouse is not running'
 
     pass
 
 class AlreadyAddedError(RenaError):
+    def __init__(self):
+        super().__init__()
     def __str__(self):
-        return 'Failed to add. The target is already in the app.'
+        return 'Failed to add. The target is already in the app \n' + self.message
 
     pass
 
 class LSLStreamNotFoundError(RenaError):
+    def __init__(self, message):
+        super().__init__(message)
     def __str__(self):
-        return 'Unable to find to LSL stream'
+        return self.message
 
-    pass
+
+class LSLChannelMismatchError(RenaError):
+    def __init__(self, message):
+        super().__init__(message)
+    def __str__(self):
+        return 'Opened stream has a different number of channels than in the settings \n' + self.message
+
+class UnsupportedErrorTypeError(RenaError):
+    def __init__(self, message):
+        super().__init__(message)
+    def __str__(self):
+        return 'This error type should not be raised \n' + self.message
