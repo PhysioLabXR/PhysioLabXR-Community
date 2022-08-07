257225326cbabf9c95a5f40eb960cb2a5ffb3de4
Ziheng(Leo) Li <s-vector.lee@hotmail.com>
2022-08-07 02:52:29 -0400

    update router dealer pattern names in networking_utils.py

--- rena/utils/networking_utils.py
+++ rena/utils/networking_utils.py
@@ -1,17 +1,17 @@
 import zmq
 
 
-def send_string_router_dealer(message, routing_id, socket_interface):
+def send_string_router(message, routing_id, socket_interface):
     socket_interface.socket.send_multipart(
         [routing_id, message.encode('utf-8')])
 
 
-def send_router_dealer(data, routing_id, socket_interface):
+def send_router(data, routing_id, socket_interface):
     socket_interface.socket.send_multipart(
         [routing_id, data])
 
 
-def recv_string_router_dealer(socket_interface, is_block):
+def recv_string_router(socket_interface, is_block):
     if is_block:
         routing_id, message = socket_interface.socket.recv_multipart(flags=0)
         return message.decode('utf-8'), routing_id
@@ -23,7 +23,7 @@ def recv_string_router_dealer(socket_interface, is_block):
         except zmq.error.Again:
             return None  # no message has arrived at the socket yet
 
-def recv_string_router_dealer_without_routing(socket_interface, is_block):
+def recv_string(socket_interface, is_block):
     if is_block:
         message = socket_interface.socket.recv_multipart(flags=0)[0]
         return message.decode('utf-8')
