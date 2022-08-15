6b71063379fe6b3805eccc167428ce920ce37a2a
ApocalyVec <zli12@wpi.edu>
2022-08-15 02:47:54 -0400

    add clear and save console message button in rena scriping;

--- rena/ui/ScriptConsoleLog.py
+++ rena/ui/ScriptConsoleLog.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from PyQt5 import QtWidgets, uic
+from PyQt5.QtCore import QMutex
 from PyQt5.QtWidgets import QLabel, QHBoxLayout
 
 from rena.config import CONSOLE_LOG_MAX_NUM_ROWS
@@ -13,13 +14,17 @@ class ScriptConsoleLog(QtWidgets.QWidget):
         self.ui = uic.loadUi("ui/ScriptConsoleLog.ui", self)
         self.log_history = ''
         self.msg_labels = []
+        self.ts_labels = []
+        self.add_msg_mutex = QMutex()
 
-        # start the play pause button
+        self.ClearLogBtn.clicked.connect(self.clear_log_btn_clicked)
+        self.SaveLogBtn.clicked.connect(self.save_log_btn_clicked)
 
     def print_msg(self, msg):
+        self.add_msg_mutex.lock()
         msg_label = QLabel(msg)
         now = datetime.now()  # current date and time
-        timestamp_string = now.strftime("%m/%d/%Y, %H:%M:%S")
+        timestamp_string = now.strftime("[%m/%d/%Y, %H:%M:%S] ")
         timestamp_label = QLabel(timestamp_string)
 
         msg_label.adjustSize()
@@ -29,7 +34,30 @@ class ScriptConsoleLog(QtWidgets.QWidget):
 
         self.LogContentLayout.addRow(timestamp_label, msg_label)
         self.msg_labels.append(msg_label)
+        self.ts_labels.append(timestamp_label)
 
         if len(self.msg_labels) > CONSOLE_LOG_MAX_NUM_ROWS:
             self.LogContentLayout.removeRow(self.msg_labels[0])
             self.msg_labels = self.msg_labels[1:]
+            self.ts_labels = self.ts_labels[1:]
+
+        self.add_msg_mutex.unlock()
+
+    def clear_log_btn_clicked(self):
+        self.add_msg_mutex.lock()
+        for msg_label in self.msg_labels:
+            self.LogContentLayout.removeRow(msg_label)
+        self.msg_labels = []
+        self.ts_labels = []
+        self.add_msg_mutex.unlock()
+
+    def save_log_btn_clicked(self):
+        filename, _ = QtWidgets.QFileDialog.getSaveFileName()
+        if filename:
+            self.add_msg_mutex.lock()
+            with open(filename, "w") as f:
+                for msg_label, ts_label in zip(self.msg_labels, self.ts_labels):
+                    f.write(ts_label.text() + msg_label.text() + '\n')
+            self.add_msg_mutex.unlock()
+
+
