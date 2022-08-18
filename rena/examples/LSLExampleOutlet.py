2e9c76848115018d465b92a2dbba4b1e76f9ab1c
ApocalyVec <zl2990@columbia.edu>
2022-08-18 13:34:44 -0400

    check script class definition when getting script classes

--- rena/examples/LSLExampleOutlet.py
+++ rena/examples/LSLExampleOutlet.py
@@ -15,7 +15,7 @@ def main(argv):
     letters = string.digits
 
     srate = 128
-    name = 'Dummy-8Chan2'
+    name = 'Dummy-8Chan'
     print('Stream name is ' + name)
     type = 'EEG'
     n_channels = 9
