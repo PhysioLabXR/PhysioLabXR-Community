6e3dfb7a9783220eb16b8bb9e80772e50df67be3
ApocalyVec <zl2990@columbia.edu>
2022-10-22 03:08:08 -0400

    update rena processing script

--- rena/inference/RenaProcessing.py
+++ rena/inference/RenaProcessing.py
@@ -9,10 +9,18 @@ class RenaProcessing(RenaScript):
     def __init__(self, *args, **kwargs):
         """
         Please do not edit this function
+
+        When the meta block changes
+        cp -> cp : train the model, report accuracy and collect more data from the next cp metablock
+        cp -> ip: train and exp
+
+        always remove the marker that has been processed from the input buffer
         """
         super().__init__(*args, **kwargs)
-        self.current_block = None
-        self.event_marker_channels = json.load(open("Presets/LSLPresets/ReNaEventMarker.json", 'r'))["ChannelNames"]
+        self.current_meta_block = None
+        self.current_condition = None
+        self.current_block_id = None
+        self.event_marker_channels = json.load(open("../Presets/LSLPresets/ReNaEventMarker.json", 'r'))["ChannelNames"]
 
     # Start will be called once when the run button is hit.
     def init(self):
@@ -20,9 +28,16 @@ class RenaProcessing(RenaScript):
 
     # loop is called <Run Frequency> times per second
     def loop(self):
-        #
-        self.inputs['Unity.ReNa.EventMarkers'][self.event_marker_channels.index("DTN")]
+        if 'Unity.ReNa.EventMarkers' in self.inputs.buffer.keys():
+            block_id_start_end = self.inputs['Unity.ReNa.EventMarkers'][0][self.event_marker_channels.index("BlockIDStartEnd"), :]
+            dtn_marker = self.inputs['Unity.ReNa.EventMarkers'][0][self.event_marker_channels.index("DTN"), :]
 
+            if np.any(block_id_start_end != 0):
+                temp = np.argwhere(block_id_start_end != 0)[0]
+                assert len(temp) == 1  # there can only be one block start give
+                self.current_block_id = block_id_start_end[temp.item]
+                print("Entered block with ID {}".format(self.current_block_id))
+                block_id_start_end[temp.item] = 0
 
     # cleanup is called when the stop button is hit
     def cleanup(self):
