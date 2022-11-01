e2bc5327a147fcb1d0592f74f9995550d7255d87
ApocalyVec <zl2990@columbia.edu>
2022-11-01 19:03:32 -0400

    update event markers

--- rena/examples/ConvertDatsInDirectoryToPickle.py
+++ rena/examples/ConvertDatsInDirectoryToPickle.py
@@ -3,10 +3,10 @@ import pickle
 
 from rena.utils.data_utils import RNStream
 
-directory = 'C:/Users/S-Vec/Downloads/post_data'
+directory = 'C:/Users/LLINC-Lab/Dropbox/ReNa/data/ReNaPilot-2022Fall/Subjects/0'
 
 files_to_convert = [os.path.join(directory, x) for x in os.listdir(directory) if x.endswith('.dats')]
-converted_file_paths = [x.replace('.dats', '_video_removed.p') for x in files_to_convert]
+converted_file_paths = [x.replace('.dats', '.p') for x in files_to_convert]
 
 for i, (f, fc) in enumerate(zip(files_to_convert, converted_file_paths)):
     print('Woring on file {} of {}'.format(i+1, len(files_to_convert)))
--- rena/inference/RenaProcessing.py
+++ rena/inference/RenaProcessing.py
@@ -5,6 +5,22 @@ import numpy as np
 from rena.scripting.RenaScript import RenaScript
 
 
+condition_name_dict = {1: "RSVP", 2: "Carousel", 3: "Visual Search", 4: "Table Search"}
+metablock_name_dict = {5: "Classifier Prep", 6: "Identifier Prep"}
+
+class ItemEvent():
+    """
+    event types can be
+    1. pop in rsvp
+    2. rotation in carousel
+    3. gaze in any condition
+    4. grab in table search
+
+    duration is a two-item tuple consisting the onset and offset time centered around the event onset
+    """
+    def __init__(self, data, dtn, event_type, duraiton, item_id, item_distance, ):
+
+
 class RenaProcessing(RenaScript):
     def __init__(self, *args, **kwargs):
         """
@@ -17,11 +33,14 @@ class RenaProcessing(RenaScript):
         always remove the marker that has been processed from the input buffer
         """
         super().__init__(*args, **kwargs)
-        self.current_meta_block = None
+        self.current_metablock = None
         self.current_condition = None
         self.current_block_id = None
         self.event_marker_channels = json.load(open("../Presets/LSLPresets/ReNaEventMarker.json", 'r'))["ChannelNames"]
         self.loop_count = 0
+
+        self.item_events = []
+        self.item_events_queue = []  # the items (timestamps and metainfo of an event) in this queue is emptied to self.item events, when its data is available
 
     # Start will be called once when the run button is hit.
     def init(self):
@@ -39,14 +58,20 @@ class RenaProcessing(RenaScript):
                 except AssertionError:
                     raise Exception("self.current_block_id is None when block_end signal comes, that means a block start is never received")
                 print("[{0}] Block with ID {1} ended. ".format(self.loop_count, self.current_block_id))
-
+            if new_meta_block:
+                self.current_block_id = new_block_id
+                print("[{0}] Entering new block with ID {1}".format(self.loop_count, metablock_name_dict[self.current_block_id]))
             if new_block_id:
                 self.current_block_id = new_block_id
-                print("[{0}] Entering new block with ID {1}".format(self.loop_count, self.current_block_id))
+                message = "[{0}] Entering new block with ID {1}".format(self.loop_count, self.current_block_id) + \
+                ". No metablock is given, assuming in practice rounds." if not self.current_metablock else ""
+                print(message)
+            if new_condition:
+                self.current_block_id = new_block_id
+                print("[{0}] Setting current condition to {1}".format(self.loop_count, condition_name_dict[self.current_block_id]))
 
             # TODO capture all the DTN events and later process them, so we can clear the event markers first
             dtn_marker = self.inputs['Unity.ReNa.EventMarkers'][0][self.event_marker_channels.index("DTN"), :]
-
             # deal with other markers based on the current condition and metablock
 
 
@@ -77,6 +102,11 @@ class RenaProcessing(RenaScript):
             block_marker = self.inputs['Unity.ReNa.EventMarkers'][0][self.event_marker_channels.index("BlockMarker"), i]
             new_condition = block_marker if block_marker <= 4 else None
             new_meta_block = block_marker if block_marker > 4 else None
+
         return new_block_id, new_meta_block, new_condition, block_end
+
+    def get_item_event(self):
+        for i in range(len(self.inputs['Unity.ReNa.EventMarkers'][1])):
+            block_id_start_end = self.inputs['Unity.ReNa.EventMarkers'][0][self.event_marker_channels.index("BlockIDStartEnd"), i]
 
 
