b1b224a0c2f9e598b4be0b30d3508b563e0e82ea
HaowenWeiJohn <hwei@wpi.edu>
2023-01-19 18:03:49 -0500

    preset update

---
+++ rena/utils/streampresets.py
@@ -0,0 +1,9 @@
+class StreamPresets:
+    def __init__(self, stream_name):
+        pass
+        self.stream_name = stream_name
+        # group_info = {}
+        self.group_info = None
+
+    def get_group_info(self):
+        pass
