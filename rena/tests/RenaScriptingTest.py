01f6c446a5e0c7f62dc982e1daf3d2815c91edc8
Ziheng 'Leo' Li <s-vector.lee@hotmail.com>
2023-01-19 17:51:00 -0500

    add unit test for scripting interface's create script

--- rena/tests/RenaScriptingTest.py
+++ rena/tests/RenaScriptingTest.py
@@ -5,47 +5,63 @@ from multiprocessing import Process
 
 import pytest
 from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QWidget
 
 from rena.MainWindow import MainWindow
 from rena.interfaces import InferenceInterface
+from rena.startup import load_default_settings
 from rena.tests.TestStream import LSLTestStream
+import importlib
 
 
 @pytest.fixture
 def app(qtbot):
-    print('Initializing test fixature for ' + 'Scripting Features')
+    print('Initializing test fixture for ' + 'Visualization Features')
     print(os.getcwd())
     # ignore the splash screen and tree icon
     app = QtWidgets.QApplication(sys.argv)
-    # main window init
-    inference_interface = InferenceInterface.InferenceInterface()
-    test_renalab_app = MainWindow(app=app, inference_interface=inference_interface)
-    test_renalab_app.show()
-    return test_renalab_app
+    # app initialization
+    load_default_settings(revert_to_default=True, reload_presets=True)  # load the default settings
+    test_renalabapp = MainWindow(app=app, ask_to_close=False)  # close without asking so we don't pend on human input at the end of each function test fixatire
+    test_renalabapp.show()
+    qtbot.addWidget(test_renalabapp)
+    return test_renalabapp
 
 
 def test_create_script(app, qtbot):
+    app.ui.tabWidget.setCurrentWidget(app.ui.tabWidget.findChild(QWidget, 'scripting_tab'))  # switch to the visualization widget
     qtbot.mouseClick(app.scripting_tab.AddScriptBtn, QtCore.Qt.LeftButton)  # click the add widget combo box
 
+    class_name = 'ScriptTest'
+    this_scripting_widget = app.scripting_tab.script_widgets[-1]
+    script_path = os.path.join(os.getcwd(), class_name + '.py')  # TODO also need to test without .py
+    this_scripting_widget.create_script(script_path, is_open_file=False)
+
+    assert os.path.exists(script_path)
+    try:
+        importlib.import_module(class_name)
+    except ImportError:
+        raise AssertionError
+
     # delete the file and remove the script from rena as clean up steps
+    qtbot.mouseClick(this_scripting_widget.removeBtn, QtCore.Qt.LeftButton)  # click the add widget combo box
+    os.remove(script_path)
 
-
-
-def test_create_script(app, qtbot):
-    test_stream_name = 'TestStopScriptRightAfterStart'
-    test_stream_name = 'TestStreamName'
-    p = Process(target=LSLTestStream, args=(test_stream_name,))
-    p.start()
-    app.lslStream_name_input.setText()
-    qtbot.mouseClick(app.add_lslStream_btn, QtCore.Qt.LeftButton)
-
-    # check all the required GUI fields are added
-    assert test_stream_name in app.stream_ui_elements
-    assert 'lsl_widget' in app.stream_ui_elements[test_stream_name]
-    app.stream_ui_elements[test_stream_name]['lsl_widget'].StreamNameLabel.text = test_stream_name
-    pass
-    # TODO
-
-# def test_label_after_click(app, qtbot):
-#     qtbot.mouseClick(app.button, QtCore.Qt.LeftButton)
-#     assert app.text_label.text() == "Changed!"
+# def test_script_input_output(app, qtbot):
+#     test_stream_name = 'TestStreamName'
+#     p = Process(target=LSLTestStream, args=(test_stream_name,))
+#     p.start()
+#
+#     app.ui.tabWidget.setCurrentWidget(app.ui.tabWidget.findChild(QWidget, 'scripting_tab'))  # switch to the visualization widget
+#     qtbot.mouseClick(app.scripting_tab.AddScriptBtn, QtCore.Qt.LeftButton)  # click the add widget combo box
+#
+#     class_name = 'ScriptTest'
+#     this_scripting_widget = app.scripting_tab.script_widgets[-1]
+#     script_path = os.path.join(os.getcwd(), class_name + '.py')
+#     this_scripting_widget.create_script(script_path, is_open_file=False)
+#
+#     # TODO add test body here
+#
+#     # delete the file and remove the script from rena as clean up steps
+#     qtbot.mouseClick(this_scripting_widget.removeBtn, QtCore.Qt.LeftButton)  # click the add widget combo box
+#     os.remove(script_path)
