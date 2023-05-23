a83b15df3be2cb07a33bae6e0afad3ab7feea7b6
HaowenWeiJohn <hwei@wpi.edu>
2023-05-22 23:55:19 -0400

    AudioDevices.py

--- rena/examples/DeviceWorkerExamples/AudioDevices.py
+++ rena/examples/DeviceWorkerExamples/AudioDevices.py
@@ -7,7 +7,7 @@ numdevices = info.get('deviceCount')
 for i in range(0, numdevices):
         if (audio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
             print("Input Device id ", i, " - ", audio.get_device_info_by_host_api_device_index(0, i).get('name'))
-            print("Input Device id ", i, " - ", audio.get_device_info_by_host_api_device_index(0, i).get('name'))
+            # print("Input Device id ", i, " - ", audio.get_device_info_by_host_api_device_index(0, i).get('name'))
 
 
 
