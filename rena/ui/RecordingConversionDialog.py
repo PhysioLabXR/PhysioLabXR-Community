e5bc7acc5cbdde50e6bbdb0cb781e1e33c24c57c
yunxiang <yp2648@columbia.edu>
2023-05-26 18:31:15 -0400

    wrap up csv converter class, implement load

--- rena/ui/RecordingConversionDialog.py
+++ rena/ui/RecordingConversionDialog.py
@@ -8,7 +8,7 @@ import numpy as np
 import os
 import csv
 
-from rena.utils.data_utils import RNStream
+from rena.utils.data_utils import RNStream, CsvStoreLoad
 
 
 class RecordingConversionDialog(QtWidgets.QWidget):
@@ -52,40 +52,6 @@ class RecordingConversionDialog(QtWidgets.QWidget):
         self.close()
         self.thread.quit()
 
-class CsvConverter:
-    def __init__(self, data):
-        self.data = data
-
-    def store_csv(self, file_path):
-        if not os.path.exists(file_path.replace('.dats', '')):
-            newfile_path = file_path.replace('.dats', '')
-            os.mkdir(newfile_path)
-        for key, value in self.data.items():
-            if value[0].ndim <= 2:
-                np.savetxt(os.path.join(newfile_path, f'{key}_y.csv'),
-                           np.append(value[0], np.reshape(value[1], (1, -1)), axis=0), delimiter=',')
-            elif key == 'monitor 0':
-                shape_0 = value[0].shape[0] * value[0].shape[2]
-                shape_1 = value[0].shape[1] * value[0].shape[3]
-                np.savetxt(os.path.join(newfile_path, f'{key}.csv'),
-                           np.reshape(value[0], (shape_0, shape_1)), delimiter=',', fmt='%d')
-                with open(os.path.join(newfile_path, f'{key}.csv'), 'a', newline='') as csvfile:
-                    writer = csv.writer(csvfile)
-                    writer.writerow(value[1])
-                    writer.writerow(value[0].shape)
-            else:
-                raise Exception(f"Unknown stream data shape")
-
-    def reload_csv(self, file_path):
-        # Open the CSV file for reading
-        with open(file_path, 'r') as file:
-            # Read the contents of the file
-            reader = csv.reader(file)
-            contents = list(reader)
-
-        # Retrieve the last row from the contents
-        last_row = contents[-1]
-
 
 
 class RecordingConversionWorker(QObject):
@@ -117,8 +83,8 @@ class RecordingConversionWorker(QObject):
             newfile_path = self.file_path.replace('.dats', '.p')
             pickle.dump(buffer, open(newfile_path, 'wb'))
         elif self.file_format == "Comma separate values (.CSV)":
-            csv_converter = CsvConverter(buffer)
-            csv_converter.store_csv(self.file_path)
+            csv_converter = CsvStoreLoad()
+            csv_converter.store_csv(buffer, self.file_path)
         else:
             raise NotImplementedError
         self.finished_conversion.emit(newfile_path)
