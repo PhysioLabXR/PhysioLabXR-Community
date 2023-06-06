99f3b2f27bf8f4b631cf1df27c509ed326fcf5dc
ApocalyVec <zl2990@columbia.edu>
2023-06-06 18:07:07 -0400

    remove AudioDeviceWidget.py, create Tobii prediction preset

--- rena/interfaces/TobiiProEyeTrackerInterface.py
+++ rena/interfaces/TobiiProEyeTrackerInterface.py
@@ -1,11 +1,16 @@
+import subprocess
 import sys
 
 from rena.interfaces.DeviceInterface import DeviceInterface
 from rena.utils.ConfigPresetUtils import DeviceType
 import tobii_research as tr
+import time
+import random
+import os
+import pylsl as lsl
+import sys
 
-
-class RenaAudioInputInterface(DeviceInterface):
+class TobiiProEyeTrackerInterface(DeviceInterface):
 
     def __init__(self, _device_name, _device_type: DeviceType):
         super().__init__(_device_name, _device_type)
@@ -49,15 +54,25 @@ class RenaAudioInputInterface(DeviceInterface):
         else:
             print("Found eyetracker with serial number " + eyetracker[0].serial_number)
             self.eye_tracker = eyetracker[0]
-            print("Address: " + eyetracker.address)
-            print("Model: " + eyetracker.model)
-            print("Name (It's OK if this is empty): " + eyetracker.device_name)
-            print("Serial number: " + eyetracker.serial_number)
+            print("Address: " + self.eye_tracker.address)
+            print("Model: " + self.eye_tracker.model)
+            print("Name (It's OK if this is empty): " + self.eye_tracker.device_name)
+            print("Serial number: " + self.eye_tracker.serial_number)
+            # set up the global variables
+        self.last_report = 0
+        self.N = 0
+        self.halted = False
 
-    # setup the global variables
-        last_report = 0
-        N = 0
-        halted = False
+        self.outlet = self.setup_lsl()
+        print("Starting gaze tracking")
+        self.start_gaze_tracking()
+
+    def stop_sensor(self):
+        pass
+
+    def process_frames(self):
+        pass
+
 
     def unpack_gaze_data(self, gaze_data):
         x = []
@@ -71,21 +86,20 @@ class RenaAudioInputInterface(DeviceInterface):
         # print(f"x: {x}")
         return x
 
-    def gase_data_callback(self, gaze_data):
+    def gaze_data_callback(self, gaze_data):
         try:
-            global last_report
-            global outlet
-            global N
-            global halted
+            # global last_report
+            # global N
+            # global halted
 
             sts = gaze_data['system_time_stamp'] / 1000000.
 
-            outlet.push_sample(self.unpack_gaze_data(gaze_data), sts)
+            self.outlet.push_sample(self.unpack_gaze_data(gaze_data), sts)
 
-            if sts > last_report + 5:
-                sys.stdout.write("%14.3f: %10d packets\r" % (sts, N))
-                last_report = sts
-            N += 1
+            if sts > self.last_report + 5:
+                sys.stdout.write("%14.3f: %10d packets\r" % (sts, self.N))
+                self.last_report = sts
+            self.N += 1
 
             # print(unpack_gaze_data(gaze_data))
         except:
@@ -95,12 +109,55 @@ class RenaAudioInputInterface(DeviceInterface):
             halted = True
 
     def start_gaze_tracking(self):
-        self.eyetracker.subscribe_to(tr.EYETRACKER_GAZE_DATA, gaze_data_callback, as_dictionary=True)
+        self.eye_tracker.subscribe_to(tr.EYETRACKER_GAZE_DATA, self.gaze_data_callback, as_dictionary=True)
         return True
+
+    def end_gaze_tracking(self):
+        self.eye_tracker.unsubscribe_from(tr.EYETRACKER_GAZE_DATA, self.gaze_data_callback)
+        return True
+
+    # def calibrate():
+    #     print("Starting calibration")
+    #     output = subprocess.check_output([tobii_et_manager_path, '\calibrate'])
+    #     print(output)
+    def setup_lsl(self):
+        # global channels
+        # global gaze_stuff
+
+        info = lsl.StreamInfo('Tobii', 'ET', self.channel_num, 90, 'float32', self.eye_tracker.address)
+        info.desc().append_child_value("manufacturer", "Tobii")
+        channels = info.desc().append_child("channels")
+        cnt = 0
+        for s in self.gaze_stuff:
+            if s[1] == 1:
+                cnt += 1
+                channels.append_child("channel") \
+                    .append_child_value("label", s[0]) \
+                    .append_child_value("unit", "device") \
+                    .append_child_value("type", 'ET')
+            else:
+                for i in range(s[1]):
+                    cnt += 1
+                    channels.append_child("channel") \
+                        .append_child_value("label", "%s_%d" % (s[0], i)) \
+                        .append_child_value("unit", "device") \
+                        .append_child_value("type", 'ET')
+
+        outlet = lsl.StreamOutlet(info)
+        return outlet
+
+
+    def halt(self):
+        global halted
+        halted = True
 
 
 if __name__ == '__main__':
-    print()
+    tobii_interface = TobiiProEyeTrackerInterface(_device_name="TobiiPro", _device_type=DeviceType.TOBIIPRO)
+    tobii_interface.start_sensor()
+    while 1:
+        tobii_interface.process_frames()
+
     # LSL example
     # info = pylsl.StreamInfo("AudioStream", "MyData", 1, 44100, pylsl.cf_int16, "myuniqueid")
     # outlet = pylsl.StreamOutlet(info)
