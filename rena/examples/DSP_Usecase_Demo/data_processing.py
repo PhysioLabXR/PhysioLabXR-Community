fe1967aae9485731ed730c8b3cd9004705104e68
HaowenWeiJohn <hw2892@columbia.edu>
2023-07-26 00:24:37 +0800

    dsp usage example

---
+++ rena/examples/DSP_Usecase_Demo/data_processing.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+from rena.utils.RNStream import RNStream
+
+recording_file_path = "C:/Users/S-Vec/Dropbox/ReNa/Data/ReNaPilot-2022Fall/Subjects/0/0.p"
+stream = RNStream(file_path=recording_file_path)
+
+
