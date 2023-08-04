6e507f078d883a1bab9becaefdfee9e52efc7b46
HaowenWeiJohn <hw2892@columbia.edu>
2023-08-05 01:02:42 +0800

    add AOIAugmentationScript.py renascript

--- rena/scripting/AOIAugmentationScript/AOIAugmentationConfig.py
+++ rena/scripting/AOIAugmentationScript/AOIAugmentationConfig.py
@@ -13,6 +13,40 @@ image_height = 1000
 image_center_x = 0
 image_center_y = 0
 
+image_shape = np.array([500, 1000])
+attention_patch_shape = np.array([20, 20])
+attention_grid_shape = np.array([25, 50])
+image_on_screen_shape = np.array([1200, 2400])
+
+
+class EventMarkerLSLOutlet:
+    StreamName = "AOIAugmentationEventMarkerLSLOutlet"
+    StreamType = "EventMarker"
+    StreamID = "1"
+    ChannelNum = 3
+    NominalSamplingRate = 1
+    BlockChannelIndex = 0
+    ExperimentStateChannelIndex = 1
+    ReportLabelChannelIndex = 2
+
+
+
+class GazeDataLSLOutlet:
+    StreamName = "TobiiProFusionUnityLSLOutlet"
+    StreamType = "GazeData"
+    StreamID = "2"
+    ChannelNum = 51
+    NominalSamplingRate = 250
+
+
+# class EventMarkerLSLInletInfo(Enum):
+#     StreamName = "AOIAugmentationEventMarkerLSLInlet"
+#     fs = None
+#     channel_count = 2
+#     channel_format = "float32"
+#
+#
+# class TobiiProFusionUnityLSLOutlet(Enum):
 
 
 class ExperimentState(Enum):
@@ -30,6 +64,14 @@ class ExperimentState(Enum):
     EndState = 12
 
 
+class ExperimentBlock(Enum):
+    InitBlock = 0
+    StartBlock = 1
+    IntroductionBlock = 2
+    PracticeBlock = 3
+    ExperimentBlock = 4
+    EndBlock = 5
+
+
 class NetworkConfig(Enum):
     ZMQPortNumber = 6667
-
--- rena/scripting/AOIAugmentationScript/AOIAugmentationScript.py
+++ rena/scripting/AOIAugmentationScript/AOIAugmentationScript.py
@@ -1,5 +1,8 @@
-from rena.scripting.AOIAugmentationScript.archives.AOIAugmentationUtils import generate_random_attention_matrix
 from rena.scripting.RenaScript import RenaScript
+from rena.scripting.AOIAugmentationScript import AOIAugmentationConfig
+from rena.scripting.AOIAugmentationScript.AOIAugmentationUtils import *
+from rena.scripting.AOIAugmentationScript.AOIAugmentationConfig import EventMarkerLSLOutlet, GazeDataLSLOutlet
+import torch
 
 
 class AOIAugmentationScript(RenaScript):
@@ -8,13 +11,25 @@ class AOIAugmentationScript(RenaScript):
         Please do not edit this function
         """
         super().__init__(*args, **kwargs)
-        self.default_attention_matrix = generate_random_attention_matrix(grid_shape=(25, 50))
+        self.currentExperimentState: AOIAugmentationConfig.ExperimentState = \
+            AOIAugmentationConfig.ExperimentState.StartState
 
+        self.currentBlock: AOIAugmentationConfig.ExperimentBlock = \
+            AOIAugmentationConfig.ExperimentBlock.StartBlock
 
+        self.currentReportLabel: int = -1
+
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+        self.attentionMatrix = AOIAttentionMatrixTorch(
+            attention_matrix=None,
+            image_shape=AOIAugmentationConfig.image_shape,
+            attention_patch_shape=AOIAugmentationConfig.attention_grid_shape,
+            device=self.device
+        )
 
     # Start will be called once when the run button is hit.
     def init(self):
-
         pass
 
     # loop is called <Run Frequency> times per second
@@ -25,16 +40,141 @@ class AOIAugmentationScript(RenaScript):
         # detected_fixation_on_display_area = [1000, 1000]
 
         # state machine
-        gaze_on_screen_area = [0.12, 0.1]
-
-
+        # gaze_on_screen_area = [0.12, 0.1]
         print('Loop function is called')
 
+        if EventMarkerLSLOutlet.StreamName not in self.inputs.keys() or GazeDataLSLOutlet.StreamName not in self.inputs.keys():
+            return
+
+        self.state_shift()
+
+        # if self.currentExperimentState == AOIAugmentationConfig.ExperimentState.:
+        #     self.practice_state()
+        #
+        # # if self.currentExperimentState == AOIAugmentationConfig.ExperimentState.PracticeState:
+        # #     self.practice_state()
+
+        if self.currentExperimentState == AOIAugmentationConfig.ExperimentState.NoAOIAugmentationState:
+            self.no_aoi_augmentation_state_callback()
+        elif self.currentExperimentState == AOIAugmentationConfig.ExperimentState.StaticAOIAugmentationState:
+            self.static_aoi_augmentation_state_callback()
+        elif self.currentExperimentState == AOIAugmentationConfig.ExperimentState.InteractiveAOIAugmentationState:
+            self.interactive_aoi_augmentation_state_callback()
+
+        # if InterruptExperimentMarker in self.inputs.get_data(P300EventStreamName)[
+        #     p300_speller_event_marker_channel_index['P300SpellerGameStateControlMarker']]:
+
+        # if InterruptExperimentMarker in self.inputs.get_data(P300EventStreamName)[
+        #     p300_speller_event_marker_channel_index['P300SpellerGameStateControlMarker']]:
+
+        # event_marker = self.inputs[EventMarkerLSLOutlet.StreamName].pop()
+
+        # if not in  #self.inputs.keys() or EventMarkerLSLOutlet not in self.inputs.keys():
+        #     return
+
+        # practice
+        #
+        #
+        #
+        #
+        # experiment
+
+        # check if we are in the static interaction mode
+
+        # state machine
+
     # cleanup is called when the stop button is hit
-
-
 
     def cleanup(self):
         print('Cleanup function is called')
 
+    def state_shift(self):
+        event_markers = self.inputs[EventMarkerLSLOutlet.StreamName].pop()
 
+        # state shift
+        for event_marker in event_markers:
+            block_marker = event_marker[AOIAugmentationConfig.EventMarkerLSLOutlet.BlockChannelIndex]
+            state_marker = event_marker[AOIAugmentationConfig.EventMarkerLSLOutlet.ExperimentStateChannelIndex]
+            report_label_marker = event_marker[AOIAugmentationConfig.EventMarkerLSLOutlet.ReportLabelChannelIndex]
+
+            if block_marker:
+                self.enter_block(block_marker)
+
+            if state_marker:
+                self.enter_state(state_marker)
+                if state_marker == AOIAugmentationConfig.ExperimentState.NoAOIAugmentationState or \
+                        state_marker == AOIAugmentationConfig.ExperimentState.StaticAOIAugmentationState or \
+                        state_marker == AOIAugmentationConfig.ExperimentState.InteractiveAOIAugmentationState:
+                    # switch to new interaction state
+                    currentReportLabel = report_label_marker
+                    print("set report interaction label to {}".format(currentReportLabel))
+
+        # AOI state machine
+        if self.currentExperimentState == AOIAugmentationConfig.ExperimentState.NoAOIAugmentationState:
+            self.no_aoi_augmentation_state_callback()
+
+        elif self.currentExperimentState == AOIAugmentationConfig.ExperimentState.StaticAOIAugmentationState:
+            self.static_aoi_augmentation_state_callback()
+
+        elif self.currentExperimentState == AOIAugmentationConfig.ExperimentState.InteractiveAOIAugmentationState:
+            self.interactive_aoi_augmentation_state_callback()
+
+        else:
+            # no interaction required
+            pass
+
+
+
+    def enter_block(self, block_marker):
+        if block_marker == AOIAugmentationConfig.ExperimentBlock.InitBlock.value:
+            self.currentBlock = AOIAugmentationConfig.ExperimentBlock.InitBlock
+        elif block_marker == AOIAugmentationConfig.ExperimentBlock.StartBlock.value:
+            self.currentBlock = AOIAugmentationConfig.ExperimentBlock.StartBlock
+        elif block_marker == AOIAugmentationConfig.ExperimentBlock.IntroductionBlock.value:
+            self.currentBlock = AOIAugmentationConfig.ExperimentBlock.IntroductionBlock
+        elif block_marker == AOIAugmentationConfig.ExperimentBlock.PracticeBlock.value:
+            self.currentBlock = AOIAugmentationConfig.ExperimentBlock.PracticeBlock
+        elif block_marker == AOIAugmentationConfig.ExperimentBlock.ExperimentBlock.value:
+            self.currentBlock = AOIAugmentationConfig.ExperimentBlock.ExperimentBlock
+        elif block_marker == AOIAugmentationConfig.ExperimentBlock.EndBlock.value:
+            self.currentBlock = AOIAugmentationConfig.ExperimentBlock.EndBlock
+        else:
+            raise ValueError('Invalid block marker')
+
+    def enter_state(self, state_marker):
+        if state_marker == AOIAugmentationConfig.ExperimentState.CalibrationState.value:
+            self.currentExperimentState = AOIAugmentationConfig.ExperimentState.CalibrationState
+        elif state_marker == AOIAugmentationConfig.ExperimentState.StartState.value:
+            self.currentExperimentState = AOIAugmentationConfig.ExperimentState.StartState
+        elif state_marker == AOIAugmentationConfig.ExperimentState.IntroductionInstructionState.value:
+            self.currentExperimentState = AOIAugmentationConfig.ExperimentState.IntroductionInstructionState
+        elif state_marker == AOIAugmentationConfig.ExperimentState.PracticeInstructionState.value:
+            self.currentExperimentState = AOIAugmentationConfig.ExperimentState.PracticeInstructionState
+        elif state_marker == AOIAugmentationConfig.ExperimentState.NoAOIAugmentationInstructionState.value:
+            self.currentExperimentState = AOIAugmentationConfig.ExperimentState.NoAOIAugmentationInstructionState
+        elif state_marker == AOIAugmentationConfig.ExperimentState.NoAOIAugmentationState.value:
+            self.currentExperimentState = AOIAugmentationConfig.ExperimentState.NoAOIAugmentationState
+        elif state_marker == AOIAugmentationConfig.ExperimentState.StaticAOIAugmentationInstructionState.value:
+            self.currentExperimentState = AOIAugmentationConfig.ExperimentState.StaticAOIAugmentationInstructionState
+        elif state_marker == AOIAugmentationConfig.ExperimentState.StaticAOIAugmentationState.value:
+            self.currentExperimentState = AOIAugmentationConfig.ExperimentState.StaticAOIAugmentationState
+        elif state_marker == AOIAugmentationConfig.ExperimentState.InteractiveAOIAugmentationInstructionState.value:
+            self.currentExperimentState = AOIAugmentationConfig.ExperimentState.InteractiveAOIAugmentationInstructionState
+        elif state_marker == AOIAugmentationConfig.ExperimentState.InteractiveAOIAugmentationState.value:
+            self.currentExperimentState = AOIAugmentationConfig.ExperimentState.InteractiveAOIAugmentationState
+        elif state_marker == AOIAugmentationConfig.ExperimentState.FeedbackState.value:
+            self.currentExperimentState = AOIAugmentationConfig.ExperimentState.FeedbackState
+        elif state_marker == AOIAugmentationConfig.ExperimentState.EndState.value:
+            self.currentExperimentState = AOIAugmentationConfig.ExperimentState.EndState
+        else:
+            raise ValueError('Invalid state marker')
+
+    def no_aoi_augmentation_state_callback(self):
+
+        pass
+
+    def static_aoi_augmentation_state_callback(self):
+        pass
+
+    def interactive_aoi_augmentation_state_callback(self):
+        pass
