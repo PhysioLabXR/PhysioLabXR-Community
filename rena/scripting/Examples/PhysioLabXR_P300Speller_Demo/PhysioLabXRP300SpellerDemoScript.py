6efd4f4ac7de97a4429a18663b6b5d8a97c42d1e
ApocalyVec <59973823+HaowenWeiJohn@users.noreply.github.com>
2023-08-30 19:50:38 -0400

    update set output function

--- rena/scripting/Examples/PhysioLabXR_P300Speller_Demo/PhysioLabXRP300SpellerDemoScript.py
+++ rena/scripting/Examples/PhysioLabXR_P300Speller_Demo/PhysioLabXRP300SpellerDemoScript.py
@@ -210,7 +210,8 @@ class PhysioLabXRGameP300SpellerDemoScript(RenaScript):
         plt.imshow(probability_matrix, cmap='hot', interpolation='nearest')
         plt.show()
 
-        self.outputs[PREDICTION_PROBABILITY_CHANNEL_NAME] = probability_matrix.flatten()
+        # self.outputs[PREDICTION_PROBABILITY_CHANNEL_NAME] = probability_matrix.flatten()
+        self.set_output(PREDICTION_PROBABILITY_CHANNEL_NAME, probability_matrix.flatten())
 
         self.data_buffer.clear_buffer_data()
 
