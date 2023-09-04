eb96037fd1bfdcd3ca456b60aa65de7d58c36116
Ziheng 'Leo' Li <s-vector.lee@hotmail.com>
2023-09-04 15:20:52 -0400

    remove splash notif for camera ports checking, to support ubuntu

--- physiolabxr/utils/video_capture_utils.py
+++ physiolabxr/utils/video_capture_utils.py
@@ -34,16 +34,16 @@ def get_working_camera_ports():
         camera = cv2.VideoCapture(dev_port)
         if not camera.isOpened():
             non_working_ports.append(dev_port)
-            SplashLoadingTextNotifier().set_loading_text("Video port %s is not working." %dev_port)
+            # SplashLoadingTextNotifier().set_loading_text("Video port %s is not working." %dev_port)
         else:
             is_reading, img = camera.read()
             w = camera.get(3)
             h = camera.get(4)
             if is_reading:
-                SplashLoadingTextNotifier().set_loading_text("Video port %s is working and reads images (%s x %s)" %(dev_port,h,w))
+                # SplashLoadingTextNotifier().set_loading_text("Video port %s is working and reads images (%s x %s)" %(dev_port,h,w))
                 working_ports.append(dev_port)
             else:
-                SplashLoadingTextNotifier().set_loading_text("Video port %s for camera ( %s x %s) is present but does not read." %(dev_port,h,w))
+                # SplashLoadingTextNotifier().set_loading_text("Video port %s for camera ( %s x %s) is present but does not read." %(dev_port,h,w))
                 available_ports.append(dev_port)
         dev_port +=1
         camera.release()
