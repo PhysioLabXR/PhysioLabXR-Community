3e5d652c87122de844eeb0978903858fed97e560
Ziheng 'Leo' Li <s-vector.lee@hotmail.com>
2023-09-11 18:56:56 -0400

    test with dummy test

--- tests/DummyTest.sh
+++ tests/DummyTest.sh
@@ -1,2 +1,7 @@
+export PYTHONPATH="$(pwd)" # add content root to PYTHONPATH
+export PYTHONPATH=$PYTHONPATH:$(pwd)/rena  # add source root to PYTHONPATH
+echo "Here is the PYTHONPATH: $PYTHONPATH"
+
+echo "Running dummy test"
 cd tests
 pytest DummyTest.py
