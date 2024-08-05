15d2cffba0e2383b1ad12bfc959dfa418d8d435e
apocalyvec <zl2990@columbia.edu>
2024-08-05 14:29:36 -0400

    add tap2char model

--- physiolabxr/scripting/illumiRead/illumiReadSwype/gaze2word/g2w_utils.py
+++ physiolabxr/scripting/illumiRead/illumiReadSwype/gaze2word/g2w_utils.py
@@ -1,5 +1,14 @@
+import re
+from collections import OrderedDict
+
 import numpy as np
 import pandas as pd
+from nltk import TreebankWordDetokenizer
+
+
+def parse_tuple(val):
+    # Remove the parentheses and split the string into individual components
+    return tuple(map(float, val.strip('()').split()))
 
 
 def load_trace_file(file_path):
@@ -23,3 +32,38 @@ def load_trace_file(file_path):
             current_array.append(row.tolist())
 
     return trace_list
+
+def parse_letter_locations(gaze_data_path):
+    letters = []
+    hit_point_local = []
+    key_ground_truth_local = []
+
+    # Read and process the lines
+    with open(gaze_data_path, 'r') as file:
+        header = file.readline()  # Read the header
+        # skip the first line and read the rest
+        for i, line in enumerate(file):
+            if re.match(r'^[a-zA-Z]', line):
+                parts = line.strip().split(',')
+                letters.append(parts[0])  # Extract the letter
+                hit_point_local.append(parse_tuple(parts[2]))
+                key_ground_truth_local.append(parse_tuple(parts[3]))
+
+    # Create a DataFrame from the extracted data
+    df = pd.DataFrame({
+        'Letter': letters,
+        'HitPointLocal': hit_point_local,
+        'KeyGroundTruthLocal': key_ground_truth_local
+    })
+
+    # for each unique letter in the letter column, get their KeyGroundTruthLocal put them in a 2d array
+    grouped = df.groupby('Letter')['KeyGroundTruthLocal']
+    letter_locations = OrderedDict()
+    for letter, group in grouped:
+        # Convert the 'KeyGroundTruthLocal' tuples to a 2D array
+        ground_truth_array = np.array(list(group))
+        assert np.all(np.std(ground_truth_array, axis=0) < np.array([5e-6,
+                                                                     5e-6])), f"std of the groundtruth is not close to zero, letter is {letter}, std is {np.std(ground_truth_array, axis=0)}"
+        letter_locations[letter] = np.mean(ground_truth_array, axis=0)
+
+    return letter_locations
