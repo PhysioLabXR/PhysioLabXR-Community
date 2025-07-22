d4e762e12a3f8d26babd725e2d3ee59488428b23
xichenhe <xh2623@columbia.edu>
2025-07-22 15:50:09 -0400

    updated the state call back and now the glance writer calls the decision tree based algorithm

--- physiolabxr/scripting/illumiRead/illumiReadSwype/illumiReadSwypeScript.py
+++ physiolabxr/scripting/illumiRead/illumiReadSwype/illumiReadSwypeScript.py
@@ -8,6 +8,9 @@ import pickle
 import sys
 import matplotlib.pyplot as plt
 from itertools import groupby
+
+from transformers.utils.import_utils import candidates
+
 # import sys, traceback
 #
 # def boom_on_exported_memoryview(unraisable):
@@ -446,7 +449,7 @@ class IllumiReadSwypeScript(RenaScript):
         elif self.currentExperimentState == illumiReadSwypeConfig.ExperimentState.KeyboardGlanceWriterState:
             self.keyboard_partialswype_state_callback()
         elif self.currentExperimentState == illumiReadSwypeConfig.ExperimentState.KeyboardDoubleCrossingState:
-            self.keyboard_partialswype_state_callback()
+            self.keyboard_glance_writer_state_callback()
 
     def gaze_pinch_state_callback(self):
         # print("keyboard click state")
@@ -695,6 +698,96 @@ class IllumiReadSwypeScript(RenaScript):
                                                      verbose=True, filter_by_starting_letter=0.35,
                                                      use_trimmed_vocab=True, njobs=16)
                     word_candidate_list = [item[0] for item in cadidate_list]
+
+                    word_candidate_list = np.array(word_candidate_list).flatten().tolist()
+                    print(word_candidate_list)
+
+                    # send the top n words to the feedback state
+                    lvt, overflow_flat = word_candidate_list_to_lvt(word_candidate_list)
+
+                    self.illumireadswype_keyboard_suggestion_strip_lsl_outlet.push_sample(lvt)
+
+
+        if self.illumiReadSwyping:
+
+            for gaze_data_t, timestamp in (
+                    zip(self.inputs[GazeDataLSLStreamInfo.StreamName][0].T,
+                        self.inputs[GazeDataLSLStreamInfo.StreamName][1])):
+                gaze_data = VarjoGazeData()
+                gaze_data.construct_gaze_data_varjo(gaze_data_t, timestamp)
+                gaze_data = self.ivt_filter.process_sample(gaze_data)
+                self.gaze_data_sequence.append(gaze_data)
+
+            self.data_buffer.update_buffers(self.inputs.buffer)
+
+        # clear the processed user input data and gaze data
+        self.inputs.clear_stream_buffer_data(GazeDataLSLStreamInfo.StreamName)
+        self.inputs.clear_stream_buffer_data(UserInputLSLStreamInfo.StreamName)
+
+    def keyboard_glance_writer_state_callback(self):
+
+        # get the user input data
+        for user_input_data_t in self.inputs[UserInputLSLStreamInfo.StreamName][0].T:
+            user_input_button_2 = user_input_data_t[
+                illumiReadSwypeConfig.UserInputLSLStreamInfo.UserInputButton2ChannelIndex]  # swyping invoker
+
+            if not self.illumiReadSwyping and user_input_button_2:
+                # start swyping
+                self.illumiReadSwyping = True
+                print("start swyping")
+                # start
+
+            if self.illumiReadSwyping and not user_input_button_2:
+                # end swyping
+                grouped_list = [list(group) for key, group in
+                                groupby(self.gaze_data_sequence, key=lambda x: x.gaze_type)]
+
+                end_fixation_trace = []
+
+                for group in grouped_list:
+                    # if the gaze is a fixation
+                    if group[0].gaze_type == GazeType.FIXATION:
+
+                        fixation_start_time = group[0].timestamp
+                        fixation_end_time = group[-1].timestamp
+
+                        # find the user input data that is closest to the fixation
+                        user_input_during_fixation_data, user_input_during_fixation_timestamps = self.data_buffer.get_stream_in_time_range(
+                            UserInputLSLStreamInfo.StreamName, fixation_start_time, fixation_end_time)
+
+                        user_input_sequence = []
+                        for user_input, timestamp in zip(user_input_during_fixation_data.T,
+                                                         user_input_during_fixation_timestamps):
+                            user_input_sequence.append(illumiReadSwypeUserInput(user_input, timestamp))
+
+                        # check if the fixation consists of only one user input
+                        for user_input in user_input_sequence:
+                            # gaze trace under fixation mode
+                            if not np.array_equal(user_input.keyboard_background_hit_point_local, [1, 1, 1]):
+                                end_fixation_trace.append(user_input.keyboard_background_hit_point_local[:2])
+
+                        pass
+
+                # reset and start decoding
+                print("start decoding swype path")
+
+                self.illumiReadSwyping = False
+                self.gaze_data_sequence = []
+                self.data_buffer = DataBuffer()
+                self.ivt_filter.reset_data_processor()
+
+                if len(end_fixation_trace) >= 1:
+                    # use the trimmed vocab to predict g2w
+                    temp_fixation_trace = np.array(end_fixation_trace)
+
+                    # predict the candidate words
+                    # cadidate_list = self.g2w.predict(4, temp_fixation_trace, run_dbscan=True, prefix=self.context,
+                    #                                  verbose=True, filter_by_starting_letter=0.35,
+                    #                                  use_trimmed_vocab=True, njobs=16)
+
+                    candidate_list = self.g2w.predict_glancewriter(4,temp_fixation_trace,prefix = self.context)
+
+                    word_candidate_list = [item[0] for item in candidate_list]
 
                     word_candidate_list = np.array(word_candidate_list).flatten().tolist()
                     print(word_candidate_list)
